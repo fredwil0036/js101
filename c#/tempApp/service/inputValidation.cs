143d366bbca57c095c6af1dcc11b8b7846bad015
FREDWIL MEMBRERE <fredwilmembrere@gmail.com>
2026-04-03 12:02:05 +0800

    validation for the inputs

---
+++ c#/tempApp/service/inputValidation.cs
@@ -0,0 +1,45 @@
+class ValidationService
+{
+    public static string GetValidName()
+    {
+        string input;
+        do
+        {
+            Console.Write("Enter wire name: ");
+            input = Console.ReadLine();
+
+              if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("❌ Error: Wire name cannot be empty. Please try again.");
+                }
+        } 
+        while (string.IsNullOrWhiteSpace(input));
+        return input;
+    }
+
+    public static double GetValidDouble(string message)
+    {
+        double value;
+        Console.Write(message);
+
+        while (!double.TryParse(Console.ReadLine(), out value))
+        {
+            Console.Write("Invalid input. Try again: ");
+        }
+
+        return value;
+    }
+
+    public static int GetValidInt(string message)
+    {
+        int value;
+        Console.Write(message);
+
+        while (!int.TryParse(Console.ReadLine(), out value))
+        {
+            Console.Write("Invalid input. Try again: ");
+        }
+
+        return value;
+    }
+}
