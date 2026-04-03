4e4d683281830acd1ddf88fd4827363794252b9c
FREDWIL MEMBRERE <fredwilmembrere@gmail.com>
2026-04-03 12:02:32 +0800

    model for the data of wire

---
+++ c#/tempApp/model/wire.cs
@@ -0,0 +1,15 @@
+
+class Wire
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Length { get; set; }
+    public int Quantity { get; set; }
+
+  
+
+    public void Display()
+    {
+        Console.WriteLine($"ID: {Id} | Wire: {Name}, Length: {Length}m, Stock: {Quantity}");
+    }
+}
